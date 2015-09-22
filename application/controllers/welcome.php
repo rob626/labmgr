af0307beec37a024827bfb3b5f855c825252e8ec
Robert Bush <rabush@us.ibm.com>
2015-09-22 19:07:23 -0400

    Rigging the controller to send to all machines

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,6 +2,10 @@
 
 class Welcome extends CI_Controller {
 
+	protected $token;
+    protected $guid;
+    private static $base = "http://%s:%s/gui/%s";
+
 	public function __construct() {
 		parent::__construct();
 		$this->load->model('room_model');
@@ -25,6 +29,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		/*
 		 $params = array(
 			'host' => '192.168.15.190',
 			'port' => '27555',
@@ -39,7 +44,7 @@ class Welcome extends CI_Controller {
 		print_r($this->utorrent->torrentRemove('BADBC42E639D870A24E70A7AAFA5389DDDDA0079'));
 		echo "</pre>";
 		die();
-		$this->load->template('welcome_message');
+		$this->load->template('welcome_message'); */
 	}
 
 	public function test() {
@@ -78,7 +83,7 @@ class Welcome extends CI_Controller {
 		print_r($this->utorrent->torrentAdd($filename));
 	}
 
-	public function getToken() {
+	public function get_token() {
 		$url = 'http://192.168.15.190:27555/gui/token.html';
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_USERPWD, 'admin' . ":" . 'web1sphere');
@@ -94,6 +99,135 @@ class Welcome extends CI_Controller {
         //$response = curl_exec($curl);
         //print_r($response);
 	}	
+
+	public function remove_all_torrents() {
+		$machines = $this->machine_model->get_machines();
+		$hash = 'BADBC42E639D870A24E70A7AAFA5389DDDDA0079';
+		$data = false;
+		foreach($machines as $machine) {
+			$this->getToken($machine['ip_address'], '27555', 'admin', 'web1sphere');
+			 $this->makeRequest($machine['ip_address'], '27555', 'admin', 'web1sphere', "?action=".($data ? "removedata" : "remove").$this->paramImplode("&hash=", $hash), false);
+		}
+	}
+
+	public function send_torrents_to_machines() {
+		$machines = $this->machine_model->get_machines();
+		$filename = '/home/robert/Desktop/Ubuntu_64-bit.torrent';
+
+		
+		foreach($machines as $machine) {
+
+			$this->getToken($machine['ip_address'], '27555', 'admin', 'web1sphere');
+
+			if($this->torrentAdd($filename, $machine['ip_address'], '27555', 'admin', 'web1sphere')) {
+				echo "Successfully sent to: " . $machine['ip_address'] . "<br>";
+			} else {
+				echo "Failed to send to: " . $machine['ip_address'] . "<br>";
+			}
+		}
+		
+
+	}
+
+	private function paramImplode($glue, $param) {
+        return $glue.implode($glue, is_array($param) ? $param : array($param));
+    }
+
+	private function makeRequest($host, $port, $user, $pass, $request, $decode = true, $options = array()) {
+        $request = preg_replace('/^\?/', '?token='.$this->token . '&', $request);
+        $ch = curl_init();
+        curl_setopt_array($ch, $options);
+        curl_setopt($ch, CURLOPT_URL, sprintf(self::$base, $host, $port, $request));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERPWD, $user.":".$pass);
+        curl_setopt($ch, CURLOPT_COOKIE, "GUID=".$this->guid);
+        $req = curl_exec($ch);
+        curl_close($ch);
+        return ($decode ? json_decode($req, true) : $req);
+    }
+	public function torrentAdd($filename, $host, $port, $user, $pass, &$estring = false) {
+        
+    $split = explode(":", $filename, 2);
+    if (count($split) > 1 && (stristr("|http|https|file|magnet|", "|".$split[0]."|") !== false)) {
+        //$this->makeRequest("?action=add-url&s=".urlencode($filename), false);
+        $decode = true;
+        //$request = preg_replace('/^\?/', '?token='.$this->token . '&', $request);
+        $request = "?token=".$this->token ."&". "action=add-url&s=".urlencode($filename);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, sprintf(self::$base, $host, $port, $request));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERPWD, $user.":".$pass);
+        curl_setopt($ch, CURLOPT_COOKIE, "GUID=".$this->guid);
+        $req = curl_exec($ch);
+        curl_close($ch);
+        return ($decode ? json_decode($req, true) : $req);
+    }
+    elseif (file_exists($filename)) {
+        //$json = $this->makeRequest("?action=add-file", true, array(CURLOPT_POSTFIELDS => array("torrent_file" => new CurlFile(realpath($filename)))));
+        echo 'Sending file: ' . $filename . "<br>";
+        $decode = true;
+        $request = "?token=".$this->token ."&". "action=add-file";
+        $ch = curl_init();
+        $args['torrent_file'] = new CurlFile($filename);
+        //curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
+        curl_setopt($ch, CURLOPT_URL, sprintf(self::$base, $host, $port, $request));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERPWD, $user.":".$pass);
+        curl_setopt($ch, CURLOPT_COOKIE, "GUID=".$this->guid);
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
+        $req = curl_exec($ch);
+        curl_close($ch);
+        $json = json_decode($req, true);
+        if (isset($json['error'])) {
+            echo $json['error'];
+            if ($estring !== false) $estring = $json['error'];
+            return false;
+        }
+        return true;
+    }
+    else {
+        echo "File doesn't exist.";
+        if ($estring !== false) $estring = "File doesn't exist!";
+        echo $estring;
+        return false; 
+    } 
+}
+	private function getToken($host, $port, $user, $pass) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, sprintf(self::$base, $host, $port, 'token.html'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERPWD, $user.":".$pass);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        $output = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        curl_close($ch);
+        $headers = substr($output, 0, $info['header_size']);
+        if (preg_match("@Set-Cookie: GUID=([^;]+);@i", $headers, $matches)) {
+            $this->guid = $matches[1];
+        }
+        if (preg_match('/<div id=\'token\'.+>(.*)<\/div>/', $output, $m)) {
+            $this->token = $m[1];
+            return true;
+        }
+        return false;
+    }
+
+	public function test_ut_lib() {
+		
+	
+	}
+
+	private function send($ip, $file) {
+		$params = array(
+				'host' => $ip,
+				'port' => '27555',
+				'user' => 'admin',
+				'pass' => 'web1sphere'
+				);
+			$this->load->library('utorrent', $params);
+			return $this->utorrent->torrentAdd($file);
+	}
 
 	/**
 	 * Add a room.
