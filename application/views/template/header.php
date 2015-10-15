8f0976d4cdc883e8f97bda106038def8a1d71c6a
Pete Neergaard <jpn@us.ibm.com>
2015-10-15 08:59:13 -0700

    change default for VMs to start by classroom

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -39,7 +39,7 @@
 		<ul class='left'>
 			<li><a href='/labmgr/add_machine'>Manage/Register</a></li>
 			<li><a href='/labmgr/push_torrents_by_classroom'>Torrents</a></li>
-			<li><a href='/labmgr/start_vms_by_machine'>VMs</a></li>
+			<li><a href='/labmgr/start_vms_by_classroom'>VMs</a></li>
 			<li><a href='/labmgr/machine_status'>Machine Status</a></li>
 			<li><a href='/labmgr/add_script'>Scripts</a></li>
 			<li><a href='/admin'>Admin</a></li>
