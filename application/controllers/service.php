1ffce9e498fa36e63e7dde5b5f800ab15062ddff
Robert Bush <rabush@us.ibm.com>
2015-10-15 06:00:54 -0400

    Adding stop all vms to by machine

--- application/controllers/service.php
+++ application/controllers/service.php
@@ -148,6 +148,9 @@ class Service extends CI_Controller {
 				$vm = $this->vm_model->get_vm($d['value']);
 				$vm = $vm[0];
 			}
+			if($d['name'] == 'stop_all') {
+				$stop_all = $d['value'];
+			}
 			if($d['name'] == 'machine_ids[]') {
 				array_push($machines, $this->machine_model->get_machine($d['value'])[0]);
 			}
@@ -157,9 +160,15 @@ class Service extends CI_Controller {
 		}
 
 		if($stop_vm) {
-			foreach($machines as $machine) {
-					$output[] = $this->vm_model->stop_vm($machine['ip_address'], $vm['path']);
+			if($stop_all == 'stop_all') {
+				foreach($machines as $machine) {
+					$output[] = $this->vm_model->stop_all_vms($machine['ip_address'], $vm['path']);
 				}
+			} else {
+				foreach($machines as $machine) {
+						$output[] = $this->vm_model->stop_vm($machine['ip_address'], $vm['path']);
+					}
+			}
 
 		} else {
 			if($start_vm_option == 'revert_start_vm' || $start_vm_option == 'revert_vm') {
