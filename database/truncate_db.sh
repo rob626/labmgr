ad2ebfb78006a8b57f793300df040539ac590d40
Robert Bush <rabush@us.ibm.com>
2015-10-18 10:40:59 -0400

    Adding backup before truncating database

--- database/truncate_db.sh
+++ database/truncate_db.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 DATABASE_NAME=labmgr
+
+mysqldump -u root $DATABASE_NAME > $(date +%Y%m%d%H%M%S).db.sql
 mysql -u root -Nse 'show tables' $DATABASE_NAME | while read table; do mysql -u root -e "truncate table $table" $DATABASE_NAME; done
