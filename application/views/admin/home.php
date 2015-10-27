c96bc18570acc0ff790d2693e1922d479c1f2afd
Pete Neergaard <jpn@us.ibm.com>
2015-10-27 07:03:06 -0700

    added placeholders for DB functions

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -2,6 +2,11 @@
 	<ul class='side-nav'>
 		<li><a href='/admin/view_watchdog_log'>View Watchdog Log</a></li>
 		<li><a href='/labmgr/'>Validate MAC / IP Mapping</a></li>
+		<hr>
+		<li><a href='/labmgr/'>Database Reset</a></li>
+		<li><a href='/labmgr/'>Database Import from Master</a></li>
+		<li><a href='/labmgr/'>Database Export</a></li>
+		<li><a href='/labmgr/'>Database Import</a></li>
 	</ul>
 	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 </div> 
