51d116b54ac29cb3e0955c3fa5cb37a05275d049
Robert Bush <rabush@us.ibm.com>
2016-01-12 10:30:39 -0500

    Changing session timeout

--- application/config/config.php
+++ application/config/config.php
@@ -245,7 +245,7 @@ $config['encryption_key'] = '9as6df97asd87sad9879fa7';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 604800;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
