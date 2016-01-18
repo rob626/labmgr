31d546be97ffba7c561e80501c3c882587b9d205
Robert Bush <rabush@us.ibm.com>
2016-01-18 17:27:05 -0500

    Adding the no auth view

---
+++ application/views/copy_file_by_machine.php
@@ -0,0 +1,61 @@
+<div class='large-2 columns side-nav-color'>
+	<ul class='side-nav'>
+		<li><a href='/labmgr/run_single_cmd_class'>Run Single Command by Class</a></li>
+		<li class='active'><a href='/labmgr/run_single_cmd_machine'>Run Single Command by Machine</a></li>
+		<hr>
+		<li><a href='/labmgr/add_script'>Manage Scripts</a></li>
+		<hr>
+		<li><a href='/labmgr/'>Copy file by Class</a></li>
+		<li><a href='/labmgr/'>Copy file by Machine</a></li>
+		<li><a href='/labmgr/'>Copy directory by Class</a></li>		
+		<li><a href='/labmgr/'>Copy directory by Machine</a></li>	
+	</ul>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</div>
+
+
+<div class='large-10 columns'>
+	<form method='POST' id='run_single_cmd_machine_form' action=''>
+	<div class='row'>
+		<div class='small-4 small-centered columns'>
+			<br>
+			<input type='submit' class='button large center' value='Make it so...'>
+			<br>
+			<input type='radio' name='start_vm_option' value='start_vm'><label>Start</label>
+			<input type='radio' name='start_vm_option' value='revert_vm'><label>Revert</label>
+			<input type='radio' checked name='start_vm_option' value='revert_start_vm'><label>Revert & Start</label>
+		</div>
+	</div>
+
+	<div class='row'>
+		<div class='large-6 columns'>
+			<h2>Run Command (ex: ls -ltr)</h2>
+			
+			<label>Command</label><input name='cmd' type='text'>
+		</div>
+
+		</div>
+
+		<div class='large-6 columns'>
+			<h2>Machines</h2>
+			<label>Show by Room</label>
+				<select id='room_filter' name="room_id">
+					<option value='-1'>All Rooms</option>
+					<?php foreach ($rooms as $room) { ?>
+					<option value='<?php echo $room['room_id'] ?>'><?php echo $room['name'] ?> </option>
+					<?php } ?>
+				</select> 
+		
+			<a href='#' id='select_all'>Select All</a>&nbsp &nbsp  <a href='#' id='unselect_all'>Unselect All</a><br>
+			<div id='machine_list'>
+			<?php
+				foreach($machines as $machine) {
+					echo "<input type='checkbox' class='checkbox' name='machine_ids[]' value='".$machine['machine_id']."'><label>Seat: ".$machine['seat']. ' ('.$machine['ip_address'].")</label><br>";
+				}
+			?>
+			</div>
+
+		</div>
+	</div>
+	</form>
+</div>
---
+++ application/views/noauth_register_machine.php
@@ -0,0 +1,107 @@
+<div class='large-2 columns side-nav-color'>
+	<ul class='side-nav'>
+		<li class='active'><a href='/labmgr/add_machine'>Add Machine</a></li>
+	</ul>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</div> 
+
+<div class='large-10 columns'>
+	<?php if(!empty($this->session->flashdata('status'))) {
+		echo "<div data-alert class='alert-box success radius'>
+  Machine Registered
+  <a href='#' class='close'>&times;</a></div>";
+	} ?>
+	<div class='row'>
+		<div class='panel'>
+			<h1>Register Machine</h1>
+		</div>
+		<form method='POST' action='/login/register_machine'>
+
+			<div class='row'>
+				<div class='large-6 columns'>
+					<label>Room</label>
+					<select id='room_filter_register_machine' name="room_id">
+						<option value='-1'>Select Room</option>
+						<?php foreach ($rooms as $room) { 
+
+							if($current_room == $room['room_id']) {
+								echo "<option selected value='" . $room['room_id']."'>" . $room['name'] ."</option>";
+
+							} else {
+								echo "<option value='" . $room['room_id']."'>" . $room['name'] ."</option>";
+
+							}
+						 } ?>
+					</select> 
+				</div>
+
+				<div class='large-6 columns'>
+					<label>Seat</label>
+					<input type='text' name='seat' value='<?php echo $next_seat; ?>'>
+				</div>
+				
+				<div class='large-6 columns'>
+					<label>IP Address</label>
+					<input type='text' name='ip_address' value='<?php echo $_SERVER['REMOTE_ADDR'] ?>'>
+				</div>
+
+				<div class='large-6 columns'>
+					<label>MAC Address</label>
+					<input type='text'  value='<?php echo $mac_guess; ?>' name='mac_address'>
+				</div>
+			</div>
+
+			<div class='row'>
+				<div class='small-4 small-centered columns'>
+					<a class='button' id='overrides' href="#">Overrides</a>
+				</div>
+			</div>
+
+			<div class='hidden row'>
+
+				<div class='large-6 columns'>
+					<label>Operating System</label>
+					<select name="os_id">
+						<?php foreach ($operating_systems as $operating_system) { ?>
+						<option value='<?php echo $operating_system['os_id'] ?>'><?php echo $operating_system['name'] ?> </option>
+						<?php } ?>
+					</select> 
+				</div>
+				
+				<div class='large-6 columns'>
+					<label>Username</label>
+					<input type='text' name='username' value='Admin'>
+				</div>
+
+			
+				<div class='large-6 columns'>
+					<label>Password</label>
+					<input type='text' name='password' value='web1sphere'>
+				</div>
+				
+				<div class='large-6 columns'>
+					<label>Torrent Client</label>
+					<select name="torrent_client_id">
+						<?php foreach ($torrent_clients as $torrent_client) { ?>
+						<option value='<?php echo $torrent_client['torrent_client_id'] ?>'><?php echo $torrent_client['name'] ?> </option>
+						<?php } ?>
+					</select> 
+				</div>
+
+				<div class='large-6 columns'>
+					<label>Transport Type</label>
+					<input type='text' name='transport_type' value='SSH'>
+				</div>
+			</div>
+
+			<div class='row'>
+				<div class='large-1 columns'>
+					<input type='submit' class='button' value='Submit'>
+				</div>
+			</form>
+			</div>
+		</div>
+
+
+
+</div>
