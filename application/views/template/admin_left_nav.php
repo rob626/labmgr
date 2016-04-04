458adea62fdba95dd8470e140dfd020c2ac9aea6
Robert Bush <rabush@us.ibm.com>
2016-04-04 18:10:55 -0400

    Adding left nav template for admin

---
+++ application/views/template/admin_left_nav.php
@@ -0,0 +1,15 @@
+<ul class='side-nav'>
+		<li><a href='/admin/set_global_defaults'>Set global defaults</a></li>
+		<li class='active'><a href='/admin/validate_ips'>Validate MAC / IP Mapping</a></li>
+		<li><a href='/admin/validate_seats'>Validate Seats</a></li>
+		<hr>
+		<li><a href='/admin/db_reset'>Database Reset</a></li>
+		<li><a href='/admin/export_db'>Database Export</a></li>
+		<li><a href='/admin/import_db'>Database Import</a></li>
+		<hr>
+		<li><a href='/admin/cleanup_watchdog'>Watchdog dropins & full cleanups</a></li>
+		<hr>
+		<li><a href='/admin/reporting_twitter'>Reporting - twitter</a></li>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+</ul>
