84f73c86c856213abfb9d6b5e4cb34536f5126c6
Robert Bush <rabush@us.ibm.com>
2016-04-07 16:35:44 -0400

    Adding twitter functionality

--- application/controllers/labmgr.php
+++ application/controllers/labmgr.php
@@ -1241,8 +1241,29 @@ class Labmgr extends MY_Controller {
 
 	}
 
+	/**
+	 * Post things to twitter.
+	 * Account: labmgrstatus
+	 */
+	public function twitterfy($message) {
+		if(strlen($message) > 140) {
+			return "Message greater than 140 characters".
+		}
+		$this->load->library('twitteroauth');
+		$connection = $this->twitteroauth->create('rcsoDfKF2RVOCSn7ciSk3ZeEn','GDBfRBG7BJkkEA77Uj0Csy0PpECvE8DHlyYHNBACA4yRcnblmm','4150443137-GfSIIyrMgo4NtSLNYFv8HrR7xxs00VDIBjaOGhR','0zhD4SkuQtNeyWCmgVfKBWKTyf4UoQgWY1YRPVJ75JWSu');
+
+		$data = array(
+			'status' => $message,
+			'in_reply_to_status_id' => 1
+			);
+		$result = $connection->post('statuses/update', $data);
+
+		return $result;
+	}
+
 	public function tester() {
-		print_r($this->machine_model->validate_seats(12));
+		
+
 
 	}
 
