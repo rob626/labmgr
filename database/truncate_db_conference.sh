619ce39231e516b235590d4696a544c23606d7da
Robert Bush <rabush@us.ibm.com>
2016-04-08 12:45:16 -0400

    Adding working directory to database scripts

--- database/truncate_db_conference.sh
+++ database/truncate_db_conference.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DATABASE_NAME=labmgr
-BACKUPFILE=/tmp/$DATABASE_NAME.$(date +%Y%m%d%H%M%S).db.sql
+BACKUPFILE=$(pwd)/database/$DATABASE_NAME.$(date +%Y%m%d%H%M%S).db.sql
 echo "Creating backup..." $BACKUPFILE
 mysqldump -u root $DATABASE_NAME > $BACKUPFILE
 echo "Truncating tables..."
-mysql -u root $DATABASE_NAME < truncate_db_conference.sql
+mysql -u root $DATABASE_NAME < $(pwd)/database/truncate_db_conference.sql
+echo "Done."
