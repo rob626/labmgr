a92d440df193ea42038f30e070ad8b3dc1819083
Pete Neergaard <jpn@us.ibm.com>
2016-05-05 12:23:15 -0400

    testing MAC/IP mapping

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -256,7 +256,7 @@ class Admin_model extends CI_Model {
         $arp_mac = shell_exec("arp -a " . $ip . " | awk '{print $4}'");
         
         if(!empty($arp_mac)) {
-            $this->logging->lwrite("Checking ip (".$ip.") and seeing [".$arp_mac."] in the arp table");
+            $this->logging->lwrite("Checking ip (".$ip.") and seeing [".trim($arp_mac)."] in the arp table");
             if(trim($arp_mac) != 'entries') {
                 // Found the IP address in the arp table.  Check the DB entry for the MAC address
                 $sql = "SELECT * FROM machine where mac_address = ?";
