d4220520d52029ae364b3c29f35ee0581e638237
Pete Neergaard <jpn@us.ibm.com>
2016-10-27 15:06:59 -0400

    the vmx path needs to be trimmed of leading whitespace, and the bulk import script can insert a leading space.  Added trim of path to the import and also for the vmrun commands

--- application/models/vm_model.php
+++ application/models/vm_model.php
@@ -84,7 +84,7 @@ class Vm_model extends CI_Model {
 	     */
 	    public function start_vm($ip, $path) {
 	    	//$path = 'C:/labs/AL1/vm/viper24vm1.vmx';
-	    	$path = str_replace('\\', '/', $path);
+	    	$path = str_replace('\\', '/', trim($path);
 	    	$dropins_dir = '/cygdrive/c/labmgr-wd/dropins/start.gui-command';
 	    	$command = 'vmrun -T ws start "'.$path.'"';
     	
@@ -111,7 +111,7 @@ class Vm_model extends CI_Model {
 	    public function revert_vm($ip, $path, $snapshot) {
 	    	$path = str_replace('\\', '/', $path);
 	    	//$path = 'C:/labs/AL1/vm/viper24vm1.vmx';
-	    	$path = str_replace('\\', '/', $path);
+	    	$path = str_replace('\\', '/', trim($path));
 	    	$dropins_dir = '/cygdrive/c/labmgr-wd/dropins/revert.gui-command';
 	    	$command = 'vmrun -T ws revertToSnapshot "'.$path. '" "'.$snapshot. '"';
 	    	
@@ -139,7 +139,7 @@ class Vm_model extends CI_Model {
 	    public function stop_vm($ip, $path) {
 	    	$path = str_replace('\\', '/', $path);
 	    	//$path = 'C:/labs/AL1/vm/viper24vm1.vmx';
-	    	$path = str_replace('\\', '/', $path);
+	    	$path = str_replace('\\', '/', trim($path));
 	    	$dropins_dir = '/cygdrive/c/labmgr-wd/dropins/stop.gui-command';
 	    	$command = 'vmrun -T ws stop "'.$path. '" hard';
 	    	
